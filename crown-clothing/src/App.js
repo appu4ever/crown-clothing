1274348d79b6b2c30eb585b147f4dc642a548cc2
Aparna <appu4ever@gmail.com>
2019-08-31 13:55:35 +0530

    Added navigation to MenuItem component using withRouter HOC

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Route , Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>HATS PAGE !!</div>
+);
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path = "/" exact component = {HomePage}/>
+        <Route path = "/shop/hats" exact component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
